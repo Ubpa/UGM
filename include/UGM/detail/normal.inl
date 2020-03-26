968fef5994b345045358419273e858e607dae21f
Ubpa <641614112@qq.com>
2020-03-27 05:01:27 +0800

    normal

---
+++ include/UGM/detail/normal.inl
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace Ubpa {
+	template<typename T>
+	const point<T, 2> normal<T>::to_sphere_texcoord() const noexcept {
+		assert(this->is_normalized());
+		// atan2: https://zh.wikipedia.org/wiki/Atan2
+		T u = (std::atan2(-(*this)[0], -(*this)[2]) + 1) / 2;
+		T v = 1 - std::acos((*this)[1]) / PI<float>;
+		return { u,v };
+	}
+
+	template<typename T>
+	const vec<T, 3> normal<T>::to_sphere_tangent() const noexcept {
+		assert(this->is_normalized());
+		// atan2: https://zh.wikipedia.org/wiki/Atan2
+		if (*this == normal{ 0, 1, 0 })
+			return { 1,0,0 };
+		else
+			return { (*this)[2], 0, (*this)[0] };
+	}
+}
--- include/UGM/normal.h
+++ include/UGM/normal.h
@@ -1,13 +1,25 @@
 ﻿#pragma once
 
+#include "point.h"
+
 #include "Interfaces/IArray/ICross.h"
-#include "Interfaces/IArray/IArray1D_Util.h"
 
 namespace Ubpa {
+	// it's necessary to distinguish vector and normal(bivector) when transformings
+	// ref
+	// - PBRT p. 86-87
+	// - wikipedia-bivector: https://en.wikipedia.org/wiki/Bivector
+	// - https://stackoverflow.com/questions/30465573/transforming-surface-normal-vectors-and-tangent-vectors
 	template<typename T>
 	struct normal : SIIT_CRTP<TemplateList<IArray1D_Util, ICross>, normal<T>, TypeList<TypeList<T, Size<3>>, T>> {
 		using SIIT_CRTP<TemplateList<IArray1D_Util, ICross>, normal<T>, TypeList<TypeList<T, Size<3>>, T>>::SIIT_CRTP;
+
+		const point<T, 2> to_sphere_texcoord() const noexcept;
+
+		const vec<T, 3> to_sphere_tangent() const noexcept;
 	};
 
 	using normalf = normal<float>;
 }
+
+#include "detail/normal.inl"

46f691cec48ff2888844136b3aa38024eab8e4e5
Ubpa <641614112@qq.com>
2020-04-14 21:07:25 +0800

    [xsimd] matrix inverse | Eric

--- include/UGM/Interfaces/IMatrix/IMatrixMul_detail.h
+++ include/UGM/Interfaces/IMatrix/IMatrixMul_detail.h
@@ -1,5 +1,134 @@
 #pragma once
 
+#pragma region Eric_inverse
+// link: https://software.intel.com/en-us/articles/optimized-matrix-library-for-use-with-the-intel-pentiumr-4-processors-sse2-instructions/
+
+#define MakeShuffleMask(x,y,z,w)           (x | (y<<2) | (z<<4) | (w<<6))
+
+// vec(0, 1, 2, 3) -> (vec[x], vec[y], vec[z], vec[w])
+#define VecSwizzleMask(vec, mask)          _mm_castsi128_ps(_mm_shuffle_epi32(_mm_castps_si128(vec), mask))
+#define VecSwizzle(vec, x, y, z, w)        VecSwizzleMask(vec, MakeShuffleMask(x,y,z,w))
+#define VecSwizzle1(vec, x)                VecSwizzleMask(vec, MakeShuffleMask(x,x,x,x))
+// special swizzle
+#define VecSwizzle_0022(vec)               _mm_moveldup_ps(vec)
+#define VecSwizzle_1133(vec)               _mm_movehdup_ps(vec)
+
+// return (vec1[x], vec1[y], vec2[z], vec2[w])
+#define VecShuffle(vec1, vec2, x,y,z,w)    _mm_shuffle_ps(vec1, vec2, MakeShuffleMask(x,y,z,w))
+// special shuffle
+#define VecShuffle_0101(vec1, vec2)        _mm_movelh_ps(vec1, vec2)
+#define VecShuffle_2323(vec1, vec2)        _mm_movehl_ps(vec2, vec1)
+
+namespace Ubpa::detail::IMatrixMul::Eric {
+    // for column major matrix
+    // we use __m128 to represent 2x2 matrix as A = | A0  A2 |
+    //                                              | A1  A3 |
+    // 2x2 column major Matrix multiply A*B
+    __forceinline __m128 Mat2Mul(__m128 vec1, __m128 vec2)
+    {
+        return
+            _mm_add_ps(_mm_mul_ps(vec1, VecSwizzle(vec2, 0, 0, 3, 3)),
+                _mm_mul_ps(VecSwizzle(vec1, 2, 3, 0, 1), VecSwizzle(vec2, 1, 1, 2, 2)));
+    }
+    // 2x2 column major Matrix adjugate multiply (A#)*B
+    __forceinline __m128 Mat2AdjMul(__m128 vec1, __m128 vec2)
+    {
+        return
+            _mm_sub_ps(_mm_mul_ps(VecSwizzle(vec1, 3, 0, 3, 0), vec2),
+                _mm_mul_ps(VecSwizzle(vec1, 2, 1, 2, 1), VecSwizzle(vec2, 1, 0, 3, 2)));
+
+    }
+    // 2x2 column major Matrix multiply adjugate A*(B#)
+    __forceinline __m128 Mat2MulAdj(__m128 vec1, __m128 vec2)
+    {
+        return
+            _mm_sub_ps(_mm_mul_ps(vec1, VecSwizzle(vec2, 3, 3, 0, 0)),
+                _mm_mul_ps(VecSwizzle(vec1, 2, 3, 0, 1), VecSwizzle(vec2, 1, 1, 2, 2)));
+    }
+
+    // Inverse function is the same no matter column major or row major
+    // this version treats it as column major
+    template<typename M>
+    inline M GetInverse(const M& inM)
+    {
+        // use block matrix method
+        // A is a matrix, then i(A) or iA means inverse of A, A# (or A_ in code) means adjugate of A, |A| (or detA in code) is determinant, tr(A) is trace
+
+        // sub matrices
+        __m128 A = VecShuffle_0101(inM[0], inM[1]);
+        __m128 C = VecShuffle_2323(inM[0], inM[1]);
+        __m128 B = VecShuffle_0101(inM[2], inM[3]);
+        __m128 D = VecShuffle_2323(inM[2], inM[3]);
+
+#if 0
+        __m128 detA = _mm_set1_ps(inM.m[0][0] * inM.m[1][1] - inM.m[0][1] * inM.m[1][0]);
+        __m128 detC = _mm_set1_ps(inM.m[0][2] * inM.m[1][3] - inM.m[0][3] * inM.m[1][2]);
+        __m128 detB = _mm_set1_ps(inM.m[2][0] * inM.m[3][1] - inM.m[2][1] * inM.m[3][0]);
+        __m128 detD = _mm_set1_ps(inM.m[2][2] * inM.m[3][3] - inM.m[2][3] * inM.m[3][2]);
+#else
+        // determinant as (|A| |C| |B| |D|)
+        __m128 detSub = _mm_sub_ps(
+            _mm_mul_ps(VecShuffle(inM[0], inM[2], 0, 2, 0, 2), VecShuffle(inM[1], inM[3], 1, 3, 1, 3)),
+            _mm_mul_ps(VecShuffle(inM[0], inM[2], 1, 3, 1, 3), VecShuffle(inM[1], inM[3], 0, 2, 0, 2))
+        );
+        __m128 detA = VecSwizzle1(detSub, 0);
+        __m128 detC = VecSwizzle1(detSub, 1);
+        __m128 detB = VecSwizzle1(detSub, 2);
+        __m128 detD = VecSwizzle1(detSub, 3);
+#endif
+
+        // let iM = 1/|M| * | X  Y |
+        //                  | Z  W |
+
+        // D#C
+        __m128 D_C = Mat2AdjMul(D, C);
+        // A#B
+        __m128 A_B = Mat2AdjMul(A, B);
+        // X# = |D|A - B(D#C)
+        __m128 X_ = _mm_sub_ps(_mm_mul_ps(detD, A), Mat2Mul(B, D_C));
+        // W# = |A|D - C(A#B)
+        __m128 W_ = _mm_sub_ps(_mm_mul_ps(detA, D), Mat2Mul(C, A_B));
+
+        // |M| = |A|*|D| + ... (continue later)
+        __m128 detM = _mm_mul_ps(detA, detD);
+
+        // Y# = |B|C - D(A#B)#
+        __m128 Y_ = _mm_sub_ps(_mm_mul_ps(detB, C), Mat2MulAdj(D, A_B));
+        // Z# = |C|B - A(D#C)#
+        __m128 Z_ = _mm_sub_ps(_mm_mul_ps(detC, B), Mat2MulAdj(A, D_C));
+
+        // |M| = |A|*|D| + |B|*|C| ... (continue later)
+        detM = _mm_add_ps(detM, _mm_mul_ps(detB, detC));
+
+        // tr((A#B)(D#C))
+        __m128 tr = _mm_mul_ps(A_B, VecSwizzle(D_C, 0, 2, 1, 3));
+        tr = _mm_hadd_ps(tr, tr);
+        tr = _mm_hadd_ps(tr, tr);
+        // |M| = |A|*|D| + |B|*|C| - tr((A#B)(D#C))
+        detM = _mm_sub_ps(detM, tr);
+
+        const __m128 adjSignMask = _mm_setr_ps(1.f, -1.f, -1.f, 1.f);
+        // (1/|M|, -1/|M|, -1/|M|, 1/|M|)
+        __m128 rDetM = _mm_div_ps(adjSignMask, detM);
+
+        X_ = _mm_mul_ps(X_, rDetM);
+        Y_ = _mm_mul_ps(Y_, rDetM);
+        Z_ = _mm_mul_ps(Z_, rDetM);
+        W_ = _mm_mul_ps(W_, rDetM);
+
+        M r;
+
+        // apply adjugate and store, here we combine adjugate shuffle and store shuffle
+        r[0] = VecShuffle(X_, Z_, 3, 1, 3, 1);
+        r[1] = VecShuffle(X_, Z_, 2, 0, 2, 0);
+        r[2] = VecShuffle(Y_, W_, 3, 1, 3, 1);
+        r[3] = VecShuffle(Y_, W_, 2, 0, 2, 0);
+
+        return r;
+    }
+}
+#pragma endregion
+
 namespace Ubpa::detail::IMatrixMul {
     template<size_t N>
     struct inverse;
@@ -45,9 +174,9 @@ namespace Ubpa::detail::IMatrixMul {
 
 #ifdef USE_XSIMD
             if constexpr (std::is_same_v<F, float>) {
-                // ref
-                // 1. https://software.intel.com/en-us/articles/optimized-matrix-library-for-use-with-the-intel-pentiumr-4-processors-sse2-instructions/
-                // 2. https://lxjk.github.io/2017/09/03/Fast-4x4-Matrix-Inverse-with-SSE-SIMD-Explained.html
+#if 1 // Eric: https://software.intel.com/en-us/articles/optimized-matrix-library-for-use-with-the-intel-pentiumr-4-processors-sse2-instructions/
+                return Eric::GetInverse(m);
+#else // intel: https://software.intel.com/en-us/articles/optimized-matrix-library-for-use-with-the-intel-pentiumr-4-processors-sse2-instructions/
                 using V = xsimd::batch<float, 4>;
                 // The inverse is calculated using "Divide and Conquer" technique. The 
                 // original matrix is divide into four 2x2 sub-matrices. Since each 
@@ -144,6 +273,9 @@ namespace Ubpa::detail::IMatrixMul {
                 V rst3 = _mm_shuffle_ps(iC, iD, 0x22);
                 return { rst0,rst1,rst2,rst3 };
             }
+#endif
+            }
+            else
 #endif // USE_XSIMD
             {
                 M rst{};
